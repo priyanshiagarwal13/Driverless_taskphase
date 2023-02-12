dc834c9c1ad0531861d8b23e323bc0d4cb6309b6
Priyanshi Agarwal <72630768+priyanshiagarwal13@users.noreply.github.com>
2023-02-12 17:09:45 +0530

    Update results.py

--- task2/results.py
+++ task2/results.py
@@ -1,8 +1,35 @@
-import matrix_operation
-object = matrix_operation.operations()
+from matrix_operations import Operations
 
-print(object.calc_inv(5))
-print(object.calc_det(15))
+if __name__== "__main__" :
+    n_s = input(" Enter the dimension of matrix A: ")
+    n = int(n_s)
+    A = [[float(input(f" Enter a element of row {(i+1)} & column {(j+1)} : ")) for j in range(n)] for i in range(n)]
+    B = [[float(input(f" Enter a element of row {(i+1)} & column {(j+1)} : ")) for j in range(n)] for i in range(n)]
+    
+    print("Input matrix A is :")
+    Operations.display((A))
 
-matrix_operation.method()
-#it shows an error if i try for the questions asked in results.py
+    print("\nThe determinent for a is :")
+    print(Operations.getMatrixDeternminant(A))
+
+    print("\nThe Adjoint for a is :")
+    Operations.display(Operations.getMatrixAdjoint(A))
+
+    print("\nThe Inverse for a is  :")
+    Operations.display(Operations.getMatrixInverse(A))
+
+    print("Input matrix B is :")
+    Operations.display((B))
+
+    print("\nThe determinent for b is :")
+    print(Operations.getMatrixDeternminant(B))
+
+    print("\nThe Adjoint for b is :")
+    Operations.display(Operations.getMatrixAdjoint(B))
+
+    print("\nThe Inverse for b is :")
+    Operations.display(Operations.getMatrixInverse(B))
+    C = [[0 for i in range(n)]for j in range(n)]
+    for i in range(n):
+        for j in range(n):
+            C[i][j] = A[i][j] * B[i][j]
